b7f32522d4521adac6591b806334824a845c0b53
mcrgbs <116827514+mcrgbs@users.noreply.github.com>
2022-10-30 21:33:44 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
-    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-    basic.pause(1000)
-    strip.rotate(1)
-    basic.pause(1000)
-    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
-    basic.pause(1000)
-    strip.rotate(1)
+    for (let index = 0; index <= 4; index++) {
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Red))
+        basic.pause(1000)
+        strip.rotate(1)
+        basic.pause(1000)
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Purple))
+        basic.pause(1000)
+        strip.rotate(1)
+    }
 })
